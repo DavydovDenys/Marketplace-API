5ac90c7a4eb02dee0fa8fbc50eec7f83630898c0
Den <denvanrain@gmail.com>
2023-11-29 14:47:54 +0200

    add home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class HomeController < ApplicationController
   def home
     render json: { message: 'Hello From Render' }
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,12 @@ Rails.application.routes.draw do
   mount Rswag::Api::Engine => '/api-docs'
 
   resources :healthz, only: %i[index create]
+
+  namespace :api do
+    namespace :v1 do
+      post 'auth/registry', to: 'authentication#registry'
+      post 'auth/refresh', to: 'authentication#refresh'
+      post 'auth/login', to: 'authentication#login'
+    end
+  end
 end
