390288d2bf5266735641923971b0f02cb7f47875
Den <denvanrain@gmail.com>
2023-11-29 15:24:44 +0200

    add RSwag

--- spec/swagger_helper.rb
+++ spec/swagger_helper.rb
@@ -15,23 +15,47 @@ RSpec.configure do |config|
   # document below. You can override this behavior by adding a swagger_doc tag to the
   # the root example_group in your specs, e.g. describe '...', swagger_doc: 'v2/swagger.json'
   config.swagger_docs = {
-    'v1/swagger.yaml' => {
-      openapi: '3.0.1',
-      info: {
-        title: 'API V1',
-        version: 'v1'
+    'v1/swagger.json' => {
+      cookie: {
+        description: 'cookie',
+        type: :apiKey,
+        name: 'cookie',
+        in: :header
       },
-      paths: {},
-      servers: [
-        {
-          url: 'https://{defaultHost}',
-          variables: {
-            defaultHost: {
-              default: 'www.example.com'
-            }
+      components: {
+        securitySchemes: {
+          Bearer: {
+            type: :apiKey,
+            name: 'Authorization',
+            in: :header
           }
         }
-      ]
+      },
+      # components: {
+      #   securitySchemes: {
+      #     cookieAuth: {
+      #       type: :apiKey,
+      #       in: :cookie,
+      #       name: 'cookies'
+      #     }
+      #   }
+      # },
+      openapi: '3.0.1',
+      info: {
+        title: 'API',
+        version: 'v1'
+      },
+      paths: {}
+      # servers: [
+      #   {
+      #     url: 'https://{defaultHost}',
+      #     variables: {
+      #       defaultHost: {
+      #         default: 'www.example.com'
+      #       }
+      #     }
+      #   }
+      # ]
     }
   }
 
@@ -39,5 +63,5 @@ RSpec.configure do |config|
   # The swagger_docs configuration option has the filename including format in
   # the key, this may want to be changed to avoid putting yaml in json files.
   # Defaults to json. Accepts ':json' and ':yaml'.
-  config.swagger_format = :yaml
+  config.swagger_format = :json
 end
